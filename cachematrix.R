040421fce8821abcc7ac8e81e07f4c6ec5eff2be
rasmarron <ras.marron@hotmail.com>
2022-11-19 21:18:15 +1100

    Committing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Pretty much a copy of makeVector except that of course we're dealing with 
+## the inverse of a matrix. So, we still define setters and getters because 
+## we're creating a "makeCacheMatrix" object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y #sets x in the parent environment
+                inv <<- NULL #effectively, clears the cache.
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse #caches the inverse
+        getinv <- function() inv # returns inverse from cache
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of the matrix created with/for the 
+## makeCacheMatrix function. However, it first checks to see if
+## the inverse has already been calculated. If so it returns the 
+## value already calculated and cached.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...) #calculating the inverse.
+        x$setinv(inv)
+        inv
 }
